733dd9b2036e7dacf511b7831fce10272150f8a5
Nicolò Cogno <niki.cogno@gmail.com>
2017-09-19 17:55:54 +0200

    Updated Converter
    Added CSV acquisition and useful openpyxl libraries dependencies

--- Converter.py
+++ Converter.py
@@ -1,7 +1,24 @@
 import pandas as pd
+from openpyxl import Workbook
+from openpyxl.worksheet.datavalidation import DataValidation
+
 
 table=pd.DataFrame.from_csv('Data.csv', sep=';', index_col=None)
 
+i=0
 
-print(table.columns.values.tolist())
-print(table)
+for column in range(table.columns.size):
+    for row in range(table[table.columns.values[column]].size):
+        print(column)
+        print(row)
+
+
+wb = Workbook()
+ws = wb.active
+
+#col_d = ws1.columns[3] # 0-indexing
+#for idx, cell in enumerate(col_d, 1):
+#    ws.cell(row=idx, col=4).value = cell.value #1-indexing
+
+
+wb.save('Data.xlsx')
