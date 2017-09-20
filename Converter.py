827653015544dd281d113f26d9fe4c1bb9dcbb11
Nicolò Cogno <niki.cogno@gmail.com>
2017-09-20 11:23:43 +0200

    Code speedup with new libraries
    Updated Converter.py using the built-in openpyxl functionalities that make it easier to convert a Pandas dataframe to an xlsx file.

--- Converter.py
+++ Converter.py
@@ -1,24 +1,14 @@
+from openpyxl.utils.dataframe import dataframe_to_rows
 import pandas as pd
 from openpyxl import Workbook
 from openpyxl.worksheet.datavalidation import DataValidation
 
-
-table=pd.DataFrame.from_csv('Data.csv', sep=';', index_col=None)
-
-i=0
-
-for column in range(table.columns.size):
-    for row in range(table[table.columns.values[column]].size):
-        print(column)
-        print(row)
-
+df=pd.DataFrame.from_csv('Data.csv', sep=';', index_col=None)
 
 wb = Workbook()
 ws = wb.active
 
-#col_d = ws1.columns[3] # 0-indexing
-#for idx, cell in enumerate(col_d, 1):
-#    ws.cell(row=idx, col=4).value = cell.value #1-indexing
+for r in dataframe_to_rows(df, index=False, header=True):
+    ws.append(r)
 
-
-wb.save('Data.xlsx')
+wb.save("pandas_openpyxl.xlsx")
